f2c8644c174990c6872a2e35d88efae533139e5f
Eben <maradebenezer@gmail.com>
2022-09-08 09:34:20 -0700

    Fixed betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
+/**
  * Function printing info about programming
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
