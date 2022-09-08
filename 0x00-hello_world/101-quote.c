4df672eac60205e3750d765ee739e496b29e1abd
Eben <maradebenezer@gmail.com>
2022-09-08 10:32:40 -0700

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Short program printing a quote
+ * Description: prints a quote
+ * Return: value of 1
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
