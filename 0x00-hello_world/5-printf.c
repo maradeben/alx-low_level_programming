09b145d2833283a3e85d9634196f6382a5073087
Eben <maradebenezer@gmail.com>
2022-09-08 09:46:12 -0700

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Short program with printf
+ * Description: use printf demo
+ * Return: value of 0 on success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
