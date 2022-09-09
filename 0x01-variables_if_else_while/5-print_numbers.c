606625aa56106bf63c54d85772c196b7cc461cb3
Eben <maradebenezer@gmail.com>
2022-09-09 15:11:03 -0700

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - short program
+ * Description:print lowercase alphabet
+ * Return: returns 0 when run sucessfully
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
