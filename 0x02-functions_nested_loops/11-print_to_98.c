1537388720473f3741149de34cc93506162938b1
Eben <maradebenezer@gmail.com>
2022-09-13 16:55:24 -0700

    Add 11-print_to 98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - count up
+ * Description: print from given number to 98
+ * @n: integer parameter
+ * Return: void returns none
+ */
+void print_to_98(int n)
+{
+	int h, t, u; /* hundreds, tens & units */
+
+	if (n < 0)
+		_putchar('-');
+	for (n; n <= 98; n++)
+	{
+		h = n / 100;
+		t = n / 10;
+		u = n % 10;
+
+		if (h != 0)
+		{
+			if (h < 0)
+				h *= -1;
+			_putchar(h + '0');
+		}
+		if (t != 0)
+		{
+			if (t < 0)
+				t *= -1;
+			_putchar(t + '0');
+		}
+		if (u != 0)
+		{
+			if (u < 0)
+				u *= -1;
+			_putchar(u + '0');
+		if (n < 98)
+		{
+			_putchar(' ');
+			_putchar(',');
+		}
+
+}
