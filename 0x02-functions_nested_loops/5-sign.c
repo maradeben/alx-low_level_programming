a3dfc4bf7cfe34e13f7bd04bf0b6e249a1129052
Eben <maradebenezer@gmail.com>
2022-09-13 14:00:56 -0700

    Added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - check size
+ * Description: checks size of number
+ * @n: a number
+ * Return: 1 if positive, 0 if 0, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+}
