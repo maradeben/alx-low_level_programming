c97b7370649af0b2e3c5eed6215436fe7e2490f5
Eben <maradebenezer@gmail.com>
2022-09-13 14:55:56 -0700

    fixed 7 again

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,10 @@
  */
 int print_last_digit(int n)
 {
-	int last;
+	int last = n % 10;
 
-	if (n < 0)
-		last = (-1 * n) % 10;
-	else
-		last = n % 10;
+	if (last < 0)
+		last *= -1;
 	_putchar(last + '0');
 	return (last);
 }
