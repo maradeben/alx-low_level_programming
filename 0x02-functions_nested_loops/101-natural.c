ba7efe64a927087d926d9bc9ae7b9da173a40373
Eben <maradebenezer@gmail.com>
2022-09-13 19:36:55 -0700

    Added 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main body
+ * Description: sum of natural numbers below 1024 that are multiples of 3 or 5
+ * Return: no return
+ */
+int main(void)
+{
+	int sum, i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0)
+				sum += i;
+	}
+	print("%d\n", i)
+}
