e72b115cf16af093eb7a2377c873a347aab34e0a
Eben <maradebenezer@gmail.com>
2022-09-15 12:15:49 -0700

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies ints
+ * @x: integer
+ * @y: integer
+ * Description: compute the product of two ints
+ * Return: sum
+ */
+int mul(int x, int y)
+{
+	return (x + y);
+}
