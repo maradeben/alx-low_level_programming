aeceb552ff1927521d011f11e541b674ab5b4869
Eben <maradebenezer@gmail.com>
2022-09-15 12:11:39 -0700

    fixed conditional

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,11 +5,11 @@
  * _isdigit - check case
  * Description: prints alphabets 10x
  * @c: an ASCII character
- * Return: 1 if it's uppercase, 0 otherwise
+ * Return: 1 if it's digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	if ((c >= '48') && (c <= '57'))
+	if ((c >= 48) && (c <= 57))
 		return (1);
 	return (0);
 }
