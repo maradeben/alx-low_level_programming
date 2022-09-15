4bae3c6c7ac24321491a8a20364e316eb271ad18
Eben <maradebenezer@gmail.com>
2022-09-15 12:52:03 -0700

    Added 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - short program
+ * Description: prints numbers 0-9
+ * Return: no return
+ */
+void more_numbers(void)
+{
+	int i, n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
