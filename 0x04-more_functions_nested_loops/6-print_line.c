9e734a006f4939e7dd7173c58528caee04177918
Eben <maradebenezer@gmail.com>
2022-09-15 13:18:41 -0700

    Adde. 6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - short program
+ * Description: print a line n * '_' long
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = n; i > 0; i--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
