e36226f8cf27bb35d47d422df489d663f346ef94
Eben <maradebenezer@gmail.com>
2022-09-19 11:02:43 -0700

    fixed 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,8 +13,13 @@ void puts2(char *str)
 
 	while (*(str + i) != '\0')
 	{
-		_putchar(*(str + i));
-		i += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+			i++;
+		}
+		else
+			i++;
 	}
 	_putchar('\n');
 }
