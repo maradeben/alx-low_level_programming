ebd5176c882881528ed3a97c8f1d2e8a27e82b9f
Eben <maradebenezer@gmail.com>
2022-09-19 09:19:26 -0700

    Added 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints
+ * Description: prints every other char in a string
+ * @str: pointer to the string
+ * Return: void returns none
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ int _strlen(char *c);
 void _puts(char *str);
 void print_rev(char *c);
 void rev_string(char *s);
+void puts2(char *str);
 
 #endif /* MAIN_H */
