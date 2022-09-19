30641125cfafcab8da8635cdc9df58fca536a834
Eben <maradebenezer@gmail.com>
2022-09-19 10:00:31 -0700

    Added 7-puts-half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - print half
+ * Description: prints second half of a string
+ * @str: pointer to the string
+ * Return: void returns none
+ */
+
+void puts_half(char *str)
+{
+	int i, count = 0;
+
+	/* first get length of string */
+	while (str[count] != 0)
+	{
+		count++;
+	}
+	/* count -= 1; */
+
+	/* next determine half of length */
+	if (count % 2 == 0)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	/* finally print second half of string */
+	while (i < count)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
