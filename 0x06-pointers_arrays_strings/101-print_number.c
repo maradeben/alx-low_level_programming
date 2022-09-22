820a854c15d7c99f61a28b58606d217a50a54a81
Eben <maradebenezer@gmail.com>
2022-09-22 11:51:43 -0700

    fixed 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,28 +8,14 @@
  */
 void print_number(int n)
 {
-	int tracker = n;
-	int place = 1, i, value;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
-		tracker *= -1;
 	}
 
-	while (tracker >= 10)
-	{
-		tracker /= 10;
-		place *= 10;
-	}
+	if (n / 10)
+		print_number(n / 10);
 
-	for (i = place; i > 1;)
-	{
-		value = n / i;
-		n %= i;
-		_putchar(value + '0');
-		i /= 10;
-	}
-	_putchar(n + '0');
+	_putchar(n % 10 + '0');
 }
