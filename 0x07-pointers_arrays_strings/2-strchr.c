70be7fe55d9d71d89ef1c330e1adab2846832f57
Eben <maradebenezer@gmail.com>
2022-09-26 14:42:29 -0700

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	int i = 0, j;
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		if (*(s + i) == c)
-		{
-			return (s + i);
-		}
-	}
+	while (*(s + i) != '\0')
+		i++;
+	for (j = 0; j <= i; j++, s++)
+		if (*s == c)
+			return (s);
 
 	return (NULL);
 }
