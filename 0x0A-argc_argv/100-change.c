923a0fd398e1e6ca78cde19775f282e185eba115
Eben <maradebenezer@gmail.com>
2022-09-30 18:04:42 -0700

    added 100-change.

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check how many coins aro needed
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int c[] = {25, 10, 5, 2, 1}; /* coins */
+	int coins = 0, i;
+	int change;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+		change = atoi(argv[1]);
+
+	if (change < 0)
+		printf("%d\n", 0);
+
+	for (i = 0; i < 5; i++)
+	{
+		if (change >= c[i] && change > 0)
+		{
+			coins += change / c[i];
+			change = change % c[i];
+		}
+	}
+
+	printf("%d\n", coins);
+	return (0);
+}
