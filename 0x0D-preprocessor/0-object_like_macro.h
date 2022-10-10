3171cc97a61c62c1db0d47c00e2b3778deb88b99
Eben <maradebenezer@gmail.com>
2022-10-10 15:28:34 -0700

    fix 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,4 @@
-#ifndef 0_OBJECT_LIKE_MACRO_H
-#define 0_OBJECT_LIKE_MACRO_H
-
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
 #define SIZE 1024
-
-#endif /* _0_OBJECT_LIKE_MACRO_H_ */
-
+#endif /* _0_OBJECT_LIKE_BUFFER */
