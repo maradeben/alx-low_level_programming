8457253f040c6f9b7b566279118e1c97a5b67881
Eben <maradebenezer@gmail.com>
2022-10-10 16:26:51 -0700

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog datatype
+ * @name: of dog
+ * @age: of dog
+ * @owner: of dog
+ *
+ * Description: represent attributes of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* _DOG_H_ */
