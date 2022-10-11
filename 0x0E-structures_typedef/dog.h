dd19b3b2ed280990868f967c50b25766abbbdb51
Eben <maradebenezer@gmail.com>
2022-10-10 17:16:47 -0700

    added typedef for struct dog in main.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,8 +16,8 @@ struct dog
 	char *owner;
 };
 
+typedf struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedf struct dog dog_t;
 
 #endif /* _DOG_H_ */
