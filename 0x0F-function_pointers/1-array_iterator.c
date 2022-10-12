4d2dd13daf8c7317ebd694208157132ac9692411
Eben <maradebenezer@gmail.com>
2022-10-12 14:48:20 -0700

    Added 1-aray_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - apply function to each element of array
+ * @array: of numbers
+ * @size: size of array
+ * @action: the function passed
+ * Return: no return
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL && size != NULL && action != NULL)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* _MAIN_H_ */
