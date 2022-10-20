b76c387afee97e19c7ff3bc1403895262222f519
Eben <maradebenezer@gmail.com>
2022-10-20 14:00:28 -0700

    added 3-add_node_end.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,9 @@
 
 #include <stddef.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -22,5 +25,6 @@ int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif /* _LISTS_H_ */
