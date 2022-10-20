f2a34f65ec8a81ae6569cd113be66ae13edd3ba9
Eben <maradebenezer@gmail.com>
2022-10-20 14:18:49 -0700

    added 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - free a list_t
+ * @head: pointer to the head
+ * Return: no return
+ */
+void free_list(list_t *head)
+{
+	list_t *the_next;
+
+	while (*head != NULL)
+	{
+		the_next = head->next;
+		free(head->str);
+		free(head->len);
+		free(head->next);
+		free(head);
+
+		head = the_next;
+	}
+}
