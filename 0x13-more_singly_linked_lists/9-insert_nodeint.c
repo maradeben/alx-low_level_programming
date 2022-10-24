ef46c1e11646115cba0924a7d622d9855034bfad
Eben <maradebenezer@gmail.com>
2022-10-24 14:19:56 -0700

    fixed 9- should insert before the current

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	for (i = 0; i < idx; i++)
+	for (i = 0; i < (idx - 1); i++)
 	{
 		current = current->next;
 		/* check for idx out of range */
