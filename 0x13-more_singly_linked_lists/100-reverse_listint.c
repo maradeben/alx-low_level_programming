91433669a7ef54f367affe43f7e902b89919d46d
Eben <maradebenezer@gmail.com>
2022-10-25 12:27:16 -0700

    fixed 100

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,7 +9,7 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL, *nxt;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 	if ((*head)->next == NULL)
 		return (*head);
