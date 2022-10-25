c56411d6ee4ebafa91b27e212c1b45e490ac73f3
Eben <maradebenezer@gmail.com>
2022-10-25 12:24:56 -0700

    fixed typo (missing ';') and betty

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,10 +9,10 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL, *nxt;
 
-	if (head == NULL) 
+	if (head == NULL)
 		return (NULL);
 	if ((*head)->next == NULL)
-		return (*head)
+		return (*head);
 
 	while (*head != NULL)
 	{
