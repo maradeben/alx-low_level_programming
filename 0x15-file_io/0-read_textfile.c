9173ab84ba5c06436575fb31bf298c37f986e44c
Eben <maradebenezer@gmail.com>
2022-11-01 09:02:35 -0700

    added 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - write to POSIX stdio
+ * @filename: text file to read
+ * @letters: no. of letters to read and print
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
+
+	if (buff == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDWR);
+	if (fd < 0)
+		return (0);
+
+	rd = read(fd, buff, letters);
+	if (rd < 0)
+		return (0);
+
+	wd = write(STDOUT_FILENO, buff, letters);
+	if (wd < 0)
+		return (0);
+
+	free(buff);
+	close(fd);
+	return (wd);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* _MAIN_H_ */
