39e03ab310e0c42fed7abcd7ea22d6111154ce2a
Eben <maradebenezer@gmail.com>
2022-11-01 10:57:41 -0700

    added 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,5 +14,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
 int append_text_to_file(const char *filename, char *text_content);
+void printerror(char *message, char *filename, int exit_value);
 
 #endif /* _MAIN_H_ */
