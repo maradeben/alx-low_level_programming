f0eab72c3717a001faf1f7f7c91b640756c46f98
Eben <maradebenezer@gmail.com>
2022-12-13 02:06:39 +0100

    Added 100, calling C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "operations.h"
+
+/**
+ * add - add two numbers together
+ * @a: first num
+ * @b: second num
+ * Return: the sum of the numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subract b from a
+ * @a: first num
+ * @b: second num
+ * Return: the difference numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: first num
+ * @b: second num
+ * Return: the product of the numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide a by b
+ * @a: first num
+ * @b: second num
+ * Return: the result of division of the numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - get modulus of numbers
+ * @a: first num
+ * @b: second num
+ * Return: the result of modular operation
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
