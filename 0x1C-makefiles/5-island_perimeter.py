de4b338ae3299e405c54ab9ba1e93b1fc4aa23a3
Eben <maradebenezer@gmail.com>
2023-05-25 04:35:28 +0100

    more robust solution

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,15 +2,30 @@
 """ get perimeter of island represent in grid """
 
 
+def immediate_perim(grid, r, c):
+    """ calculate immediate perimeter of current land block """
+    count = 0
+    if grid[r][c-1] == 0:
+        count += 1
+    if grid[r][c+1] == 0:
+        count += 1
+    if grid[r-1][c] == 0:
+        count += 1
+    if grid[r+1][c] == 0:
+        count += 1
+    return (count)
+
+
 def island_perimeter(grid):
-    found = 0
+    """ calculate the perimeter of island """
+    perimeter = 0
     for r in range(1, len(grid)):
         for c in range(1, len(grid[0])):
             if grid[r][c] == 1:
-                found += 1
+                perimeter += immediate_perim(grid, r, c)
                 if grid[r][c+1] == 1:
                     continue
                 elif grid[r+1][c] == 1:
                     r += 1
                     break
-    return (found * 2 + 2)
+    return (perimeter)
